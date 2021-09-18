bbc5318abc7d9c014d89a57ce8bfabe9760cb608
Denys André dos Santos Lins <denyslins@gmail.com>
2021-09-18 12:18:54 -0300

    Fix smallest_subarray_with_given_sum

--- patterns-for-coding-interview/sliding-window/2_smallest_subarray_with_given_sum/solution.py
+++ patterns-for-coding-interview/sliding-window/2_smallest_subarray_with_given_sum/solution.py
@@ -15,6 +15,8 @@ def smallest_subarray_with_given_sum(s, arr):
                 _min_len = _window_end - _window_start
             _sum -= arr[_window_start]
             _window_start += 1
+    if _min_len == sys.maxsize:
+      _min_len = 0
 
     return _min_len
 
@@ -25,3 +27,5 @@ print("Smallest subarray length: {}".format(
     smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 8])))
 print("Smallest subarray length: {}".format(
     smallest_subarray_with_given_sum(8, [2, 1, 5, 2, 3, 2])))
+print("Smallest subarray length: {}".format(
+    smallest_subarray_with_given_sum(200, [2, 1, 5, 2, 3, 2])))
