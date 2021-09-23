811e5e014002099d3d698db96b9e1cc2166bdf24
Denys André dos Santos Lins <denyslins@gmail.com>
2021-09-23 18:06:30 -0300

    977-squares-of-a-sorted-array

---
+++ leetcode/977-squares-of-a-sorted-array/solution.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/squares-of-a-sorted-array/
+
+from typing import List
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        i = 0
+        j = len(nums) - 1
+        result = []
+        while i <= j:
+            t1 = nums[i] ** 2
+            t2 = nums[j] ** 2
+            if t1 < t2:
+                result.insert(0, t2)
+                j -= 1
+            else:
+                result.insert(0, t1)
+                i += 1
+
+        return result
+
+
+if __name__ == "__main__":
+    print(Solution().sortedSquares([-4, -1, 0, 3, 10]))
+    print(Solution().sortedSquares([-7, -3, 2, 3, 11]))
