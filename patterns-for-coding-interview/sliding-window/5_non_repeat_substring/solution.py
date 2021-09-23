6ae9f65745254be9e4a0d5eaae024a3e9ee4157a
Denys André dos Santos Lins <denyslins@gmail.com>
2021-09-23 18:07:10 -0300

    5_non_repeat_substring

---
+++ patterns-for-coding-interview/sliding-window/5_non_repeat_substring/solution.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+
+def non_repeat_substring(s: str) -> int:
+    tmp = {}
+    start, end, longest = 0, 0, 0
+    for i in s:
+        if i not in tmp:
+            tmp[i] = 1
+            longest = max(longest, end - start + 1)
+        else:
+            tmp[i] += 1
+            while max(tmp.values()) > 1:
+                if tmp[s[start]] > 1:
+                    tmp[s[start]] -= 1
+                else:
+                    tmp.pop(s[start])
+                start += 1
+        end += 1
+
+    return longest
+
+
+print(non_repeat_substring("abcabcbb"))
+print(non_repeat_substring("bbbbb"))
+print(non_repeat_substring("pwwkew"))
+print(non_repeat_substring(""))
+print(non_repeat_substring("dvdf"))
