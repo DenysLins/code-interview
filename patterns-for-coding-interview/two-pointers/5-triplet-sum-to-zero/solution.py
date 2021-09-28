9e37ba2b4445d4fb2fbb4d3a7d02c7618240f63f
Denys André dos Santos Lins <denyslins@gmail.com>
2021-09-28 19:05:02 -0300

    Fix 5-triplet-sum-to-zero

--- patterns-for-coding-interview/two-pointers/5-triplet-sum-to-zero/solution.py
+++ patterns-for-coding-interview/two-pointers/5-triplet-sum-to-zero/solution.py
@@ -7,16 +7,13 @@ class Solution:
     def searchTriplets(self, nums: List[int]) -> List[int]:
         result = []
         nums.sort()
-        for i in range(len(nums) - 1):
+        for i in range(len(nums) - 2):
             j = i + 1
             k = len(nums) - 1
-            target = -nums[i]
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            while j <= k:
-                if nums[j] + nums[k] == target and [-target, nums[j], nums[k]] not in result:
-                    result.append([-target, nums[j], nums[k]])
-                if nums[j] + nums[k] > target:
+            while j < k:
+                if i != j and i != k and j != k and nums[i] + nums[j] + nums[k] == 0 and [nums[i], nums[j], nums[k]] not in result:
+                    result.append([nums[i], nums[j], nums[k]])
+                if nums[i] + nums[j] + nums[k] > 0:
                     k -= 1
                 else:
                     j += 1
@@ -27,3 +24,4 @@ class Solution:
 if __name__ == "__main__":
     print(Solution().searchTriplets([-3, 0, 1, 2, -1, 1, -2]))
     print(Solution().searchTriplets([-5, 2, -1, -2, 3]))
+    print(Solution().searchTriplets([0, 0, 0]))
